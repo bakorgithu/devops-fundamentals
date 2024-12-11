46feda13c76643db0f2155a4b3b8eb84e3168980
bakorgithu <bakorivoeyong237@gmail.com>
2024-12-11 10:36:50 +0100

    Updated main.tf

--- terraform_ec2instance/main.tf
+++ terraform_ec2instance/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "test_ec2" {
   instance_type   = var.instance_type
   key_name        = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instanceprofile.name
 
 
   tags = {
