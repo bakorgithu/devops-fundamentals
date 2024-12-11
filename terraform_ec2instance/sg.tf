6cce19f694edad90b72daf71eb5cff3886c080d3
bakorgithu <bakorivoeyong237@gmail.com>
2024-12-11 10:39:44 +0100

    Updated sg.tf

--- terraform_ec2instance/sg.tf
+++ terraform_ec2instance/sg.tf
@@ -19,5 +19,5 @@ cidr_ipv4           = "0.0.0.0/0"
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+ip_protocol       = "-1" 
 }
