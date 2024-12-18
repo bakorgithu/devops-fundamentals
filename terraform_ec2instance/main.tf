58e8f1b4d2db46b50430881f6a776a1cc482e96a
bakorgithu <bakorivoeyong237@gmail.com>
2024-12-18 12:56:19 +0100

    Updated main.tf

--- terraform_ec2instance/main.tf
+++ terraform_ec2instance/main.tf
@@ -1,6 +1,15 @@
 
 # defining the EC2 Instance
 
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "test_ec2" {
   ami             = var.ami_id
   instance_type   = var.instance_type
